52a944386b9e235f6e29cc4655a39fa3ab3a9cd2
ojasve <ojasvegarg@gmail.com>
2017-11-23 19:54:36 +0530

    view layer in project

--- ruby_on_rails/hospital_management_system/app/controllers/home_controller.rb
+++ ruby_on_rails/hospital_management_system/app/controllers/home_controller.rb
@@ -1,9 +1,6 @@
 class HomeController < ApplicationController
 
 	def index
-		@doctors = Doctor.all
-    @doctors.each do |i| 
-    	puts i
-    end
+		@appointments = Appointment.where("Date(schedule) = #{Date.today}")
 	end
 end
