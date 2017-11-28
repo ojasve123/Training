3b2908d61d6c221a2e17c2bab510811f73e21a17
ojasve <ojasvegarg@gmail.com>
2017-11-28 17:48:40 +0530

    updating web page hospital management system

--- ruby_on_rails/hospital_management_system/app/controllers/doctor_controller.rb
+++ ruby_on_rails/hospital_management_system/app/controllers/doctor_controller.rb
@@ -7,7 +7,7 @@ class DoctorController < ApplicationController
     @create_doctor_form = Doctor.find(params[:id])
   end
   def insert_doctor_form
-    Doctor.create(doctor_name: params[:doctor_name],address: params[:address], gender: params[:gender], department_id: params[:department_id]) 
+    Doctor.create(doctor_name: params[:doctor_name],address: params[:address],date_of_joining: params[:date_of_joining], gender: params[:gender], department_id: params[:department_id]) 
     redirect_to root_path
   end
 end
