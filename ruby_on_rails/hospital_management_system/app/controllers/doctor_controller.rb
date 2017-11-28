849c899c022923dc7f696131661f57004f6497b7
ojasve <ojasvegarg@gmail.com>
2017-11-28 10:49:38 +0530

    modifying database

--- ruby_on_rails/hospital_management_system/app/controllers/doctor_controller.rb
+++ ruby_on_rails/hospital_management_system/app/controllers/doctor_controller.rb
@@ -7,8 +7,8 @@ class DoctorController < ApplicationController
       @create_doctor_form=Doctor.find(params[:id])
   end
   def insert_doctor_form
-  	
-  	Doctor.create(doctor_name: params[:doctor_name],address: params[:address], gender: params[:gender],department_id: params[:department_id]) 
+  	debugger
+  	Doctor.create(doctor_name: params[:doctor_name],address: params[:address], gender: params[:gender]) 
     redirect_to root_path
   end
 end
