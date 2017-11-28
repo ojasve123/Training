2eac84c40530e2927f4f4b18330ec9023b6ee6b9
ojasve <ojasvegarg@gmail.com>
2017-11-28 15:50:14 +0530

    updating web page with links

--- ruby_on_rails/hospital_management_system/app/controllers/patient_controller.rb
+++ ruby_on_rails/hospital_management_system/app/controllers/patient_controller.rb
@@ -1,16 +1,13 @@
 class PatientController < ApplicationController
-skip_before_action :verify_authenticity_token 
-
-	def show_details
+  skip_before_action :verify_authenticity_token 
+  def show_details
     @patient = Patient.find(params[:id])
 	end
-
   def create_patient
-    @create_patient_form=Patient.find(params[:id])
-  end
-  
+    @create_patient_form = Patient.find(params[:id])
+  end 
   def insert_patient_form
-    Patient.create(patient_name: params[:patient_name],address: params[:address],date_of_joining: params[:date_of_joining], disease_history: params[:disase_history]) 
+    Patient.create(patient_name: params[:patient_name],address: params[:address],date_of_joining: params[:date_of_joining], disease_history: params[:disease_history]) 
     redirect_to root_path
   end
 end
--- ruby_on_rails/hospital_management_system/config/routes.rb
+++ ruby_on_rails/hospital_management_system/config/routes.rb
@@ -1,11 +1,8 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-
   root 'home#index'
-
   get "/patient/:id", to: "patient#show_details", as: "patient_details"
   get "/doctor/:id", to: "doctor#show_details1", as: "doctor_details"
-
   get "/create_doctor", to: "doctor#create_doctor_form", as: "create_doctor"
   get "/create_patient", to: "patient#create_patient_form", as: "create_patient"
   post "/create_patient_form", to:"patient#insert_patient_form", as:"insert_patient"
